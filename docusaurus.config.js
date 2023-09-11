4abbfd904ebd380db7a8d484c3f8351acc765652
RaRoyce៚ <theara2048@gmail.com>
2023-09-11 10:01:48 +0700

    changed name add logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Domnossrai',
+  tagline: 'Domnossrai are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -64,10 +64,10 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Domnossrai',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'Domnossrai Logo',
+          src: 'img/logo.png',
         },
         items: [
           {
